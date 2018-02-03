9bf125d55642a51a786f115203fc7c6781aea8cc
Wayne Patterson <Wayne.Patterson@nextgearcapital.com>
2018-02-03 08:42:23 -0500

    add apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'https://3wzp7qnjv.lp.gql.zone/graphql' }),
+  cache: new InMemoryCache()
+});
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+render(<Root />, document.getElementById('root'));
 registerServiceWorker();
